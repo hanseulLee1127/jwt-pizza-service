dccfcb3e5d85f08db22f7fbc92f27584e203e804
이한슬 <hanseul@ihanseul-ui-MacBookPro.local>
2025-03-18 21:12:56 -0600

    fix lint error

--- src/routes/metric.test.js
+++ src/routes/metric.test.js
@@ -1,14 +1,16 @@
-const metrics = require("../metrics.js")
-
+const metrics = require("../metrics.js");
 
 test("cpu_usage", async () => {
     let answer = metrics.getCpuUsagePercentage();
-
     expect(typeof answer).toBe("number");
-})
+});
 
 test("memory_usage", async () => {
     let answer = metrics.getMemoryUsagePercentage();
-
     expect(typeof answer).toBe("string");
-})
+});
+
+// 모든 테스트 종료 후 실행
+afterAll(() => {
+    metrics.stopMetricsCollection();
+});
