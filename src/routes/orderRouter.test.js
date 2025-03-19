07b24d0784c7e4ae999d2e9097301667370b3cc9
이한슬 <hanseul@ihanseul-ui-MacBookPro.local>
2025-03-18 21:04:02 -0600

    fix lint error

--- src/routes/orderRouter.test.js
+++ src/routes/orderRouter.test.js
@@ -2,91 +2,92 @@ const request = require("supertest");
 const app = require("../service");
 const { Role, DB } = require("../database/database.js");
 
+let testUserAuthToken;
 
-let adminAuthToken;
-let adminUser;
-let regularUser;
-let menuItem;
-let order;
+beforeAll(async () => {
+  const testUser = await createAdminUser();
+  const loginRes = await request(app).put("/api/auth").send(testUser);
+  testUserAuthToken = loginRes.body.token;
+  expectValidJwt(testUserAuthToken);
+});
+
+//test getMenu endpoint
+test("getMenu", async () => {
+  const addMenuItemResult = await request(app)
+    .get("/api/order/menu")
+    .set("Authorization", `Bearer ${testUserAuthToken}`);
+
+  expect(addMenuItemResult.status).toBe(200);
+  expect(addMenuItemResult.body).toBeInstanceOf(Array);
+});
+
+//test addMenuItem endpoint
+test("addMenuItem", async () => {
+  const menuItem = {
+    title: "new pizza",
+    description: "new pizza description",
+    image: "image9.png",
+    price: 1.5,
+  };
+
+  const addMenuItemResult = await request(app)
+    .put("/api/order/menu")
+    .send(menuItem)
+    .set("Authorization", `Bearer ${testUserAuthToken}`);
+
+  expect(addMenuItemResult.status).toBe(200);
+  expect(addMenuItemResult.body).toBeInstanceOf(Array);
+});
+
+//test getOrders endpoint
+test("getOrders", async () => {
+  const getOrdersResult = await request(app)
+    .get("/api/order")
+    .set("Authorization", `Bearer ${testUserAuthToken}`);
+
+  expect(getOrdersResult.status).toBe(200);
+  expect(getOrdersResult.body).toBeInstanceOf(Object);
+  return;
+});
+
+//test createOrders endpoint
+test("createOrders", async () => {
+    const createOrderWithOutAuth = await request(app)
+    .post("/api/order")
+    .send()
+
+    expect(createOrderWithOutAuth.status).toBe(401);
+    
+});
+
+test("createOrdersWithAuth", async () => {
+  const exampleOrder = {"franchiseId": 1, "storeId":1, "items":[{ "menuId": 1, "description": "Veggie", "price": 0.05 }]}
+  const getOrdersResult = await request(app)
+  .post("/api/order")
+  .set("Authorization", `Bearer ${testUserAuthToken}`)
+  .set("Content-Type", "application/json")
+  .send(exampleOrder);
+
+  expect(getOrdersResult.status).toBe(200);
+
+});
+
+//Helper functions to create random Names and Admin users etc.
+function randomName() {
+  return Math.random().toString(36).substring(2, 12);
+}
 
 async function createAdminUser() {
   let user = { password: "toomanysecrets", roles: [{ role: Role.Admin }] };
   user.name = randomName();
   user.email = user.name + "@admin.com";
 
-  await DB.addUser(user);
-
-  user.password = "toomanysecrets";
-  return user;
+  user = await DB.addUser(user);
+  return { ...user, password: "toomanysecrets" };
 }
 
-function randomName() {
-  return Math.random().toString(36).substring(2, 12);
+function expectValidJwt(potentialJwt) {
+  expect(potentialJwt).toMatch(
+    /^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/
+  );
 }
-
-if (process.env.VSCODE_INSPECTOR_OPTIONS) {
-  jest.setTimeout(60 * 1000 * 5); 
-}
-
-beforeAll(async () => {
-  regularUser = await {
-    name: "pizza diner",
-    email: "reg@test.com",
-    password: "a",
-  };
-
-  regularUser.email = Math.random().toString(36).substring(2, 12) + "@test.com";
-  adminUser = await createAdminUser();
-
-  const userLoginRes = await request(app).post("/api/auth").send(regularUser);
-  const loginRes = await request(app).put("/api/auth").send(adminUser);
-  adminAuthToken = loginRes.body.token;
-  console.log("Admin Login Res: ", loginRes.body);
-  console.log("User Login Res: ", userLoginRes.body);
-
-  menuItem = {
-    title: "Student",
-    description: "No topping, no sauce, just carbs",
-    image: "pizza9.png",
-    price: 0.0001,
-  };
-});
-
-test("Get Menu", async () => {
-  const res = await request(app).get("/api/order/menu");
-  expect(res.status).toBe(200);
-  expect(res.body).toBeInstanceOf(Array);
-});
-
-test("Add Menu Item", async () => {
-  const res = await request(app)
-    .put("/api/order/menu")
-    .set("Authorization", `Bearer ${adminAuthToken}`)
-    .send(menuItem);
-  console.log(res.body);
-  expect(res.status).toBe(200);
-});
-
-test("Create and Get Order", async () => {
-  order = {
-    franchiseId: 1,
-    storeId: 1,
-    items: [{ menuId: 1, description: "Veggie", price: 0.05 }],
-  };
-  console.log("Order", order);
-  const res = await request(app)
-    .post("/api/order")
-    .set("Authorization", `Bearer ${adminAuthToken}`)
-    .send(order);
-  console.log("Order of the body", res.body);
-  if (res.status !== 200) {
-    console.error("Error creating order:", res.body);
-  }
-  expect(res.status).toBe(200);
-
-  const getRes = await request(app)
-    .get("/api/order")
-    .set("Authorization", `Bearer ${adminAuthToken}`);
-  console.log(getRes.body);
-  expect(getRes.status).toBe(200);
-});
